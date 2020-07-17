5d34d88c47d42340ae2ba96f09753ffe2d7ba1db
Cameron McGinley <49362691+cameronmcginley@users.noreply.github.com>
2020-07-16 19:36:03 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,3 @@
-jQuery(document).ready(function($) {
-  $(".theme").click(function() {
-	$(".theme").toggleClass("active");
-	$("").toggleClass("dark-mode");
-	$.cookie("theme", $(".theme").hasClass('active'));
-  });
-
-  if ($.cookie("theme") == "true") {
-	$(".theme").addClass("active");
-	$("body").addClass("dark-mode");
-  }
-});
-
 (function($) {
 
 	var	$window = $(window),
