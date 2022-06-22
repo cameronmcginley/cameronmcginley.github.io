d54097714de52c5565bb453c90d4879092e1d6f1
Cameron McGinley <mcginleycameron@gmail.com>
2022-06-21 21:09:52 -0400

    Added seperate gh and public project links

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,53 +1,76 @@
 import React, { useState } from "react";
-import { Link, Box } from '@mui/material';
+import { Link, Box } from "@mui/material";
 
 const ProjectCard = (props) => {
     return (
-    <div className="projectCard">
-        <Box 
-            className="projectCard-image-div"
-            sx={{ 
-                transform: 'translateZ(0)', 
-                overflow: 'hidden', 
-                border: "1px solid black",
-                width: '400%',
-                height: '100%',
-                maxWidth: '20rem',
-                maxHeight: '20rem',
-            }}
-        >
-            <Link href={props.cardData.link}>
-                <Box
-                    className="projectCard-image"
-                    component="img"
-                    sx={{
-                        display: 'block',
-                        width: '100%',
-                        height: 'auto',
-                        transition: '0.2s',
-                        "&:hover": {
-                            transform: 'scale(1.1)',
-                            overflow: 'hidden',
-                        }
-                    }}
-                    alt="Project Image" 
-                    src={"images/" + props.cardData.image} 
-                />
-            </Link>
-        </Box>
+        <div className="projectCard">
+            <Box
+                className="projectCard-image-div"
+                sx={{
+                    transform: "translateZ(0)",
+                    overflow: "hidden",
+                    border: "1px solid black",
+                    width: "400%",
+                    height: "100%",
+                    maxWidth: "20rem",
+                    maxHeight: "20rem",
+                }}
+            >
+                <Link href={props.cardData.gh_link}>
+                    <Box
+                        className="projectCard-image"
+                        component="img"
+                        sx={{
+                            display: "block",
+                            width: "100%",
+                            height: "auto",
+                            transition: "0.2s",
+                            "&:hover": {
+                                transform: "scale(1.1)",
+                                overflow: "hidden",
+                            },
+                        }}
+                        alt="Project Image"
+                        src={"images/" + props.cardData.image}
+                    />
+                </Link>
+            </Box>
 
-        <div className="projectCard-info">
-            <Link href={props.cardData.link}><h3>{props.cardData.title}</h3></Link>
-            <p>{props.cardData.description}</p>
+            <div className="projectCard-info">
+                <Link href={props.cardData.gh_link}>
+                    <h3>{props.cardData.title}</h3>
+                </Link>
 
-            <div className="projectCard-attributes">
-                {props.cardData.tools.map((tool) => (
-                    <Link href={tool.link}><p>{tool.text}</p></Link>
-                ))}
+                <div className="projectCard-projectLinks">
+                    <div className="projectCard-projectLink">
+                        <h4>Github: </h4>
+                        <Link href={props.cardData.gh_link}>
+                            <p>{props.cardData.gh_link}</p>
+                        </Link>
+                    </div>
+
+                    {props.cardData.project_link && (
+                        <div className="projectCard-projectLink">
+                            <h4>Public App: </h4>
+                            <Link href={props.cardData.project_link}>
+                                <p>{props.cardData.project_link}</p>
+                            </Link>
+                        </div>
+                    )}
+                </div>
+
+                <p>{props.cardData.description}</p>
+
+                <div className="projectCard-attributes">
+                    {props.cardData.tools.map((tool) => (
+                        <Link href={tool.link}>
+                            <p>{tool.text}</p>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-    )
-}
+    );
+};
 
 export default ProjectCard;
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -9,7 +9,9 @@ const Projects = (props) => {
             <ProjectCard
                 cardData={{
                     title: "Course Sign-In Dashboard App",
-                    link: "https://github.com/cameronmcginley/CourseDashboard/",
+                    project_link: "https://coursedashboard.web.app/",
+                    gh_link:
+                        "https://github.com/cameronmcginley/CourseDashboard/",
                     description:
                         "This web app serves as a dashboard for courses and attendees. It provides an easy to use back-end for course instructors or administrators to handle course data and attendee data. One primary feature is the course tracker, where instructors can input their courses to automatically generate QR codes. These QR codes allow attendees to sign into courses by scanning them, either via the webpage or a printable page. This tracker also displays a unique page for each course with various helpful info and features. The next main feature is the attendee/sign-in data, where all sign-ins are stored. This data can be exported to .CSV, and comes with wide support for querying data. This project uses Firebase's Cloud Firestore as a database. More information can be found on the linked GitHub.",
                     image: "project-coursesignindashboard-image.png",
@@ -41,7 +43,10 @@ const Projects = (props) => {
             <ProjectCard
                 cardData={{
                     title: "Sorting Visualizer",
-                    link: "https://github.com/cameronmcginley/sorting-visualizer/",
+                    project_link:
+                        "https://cameronmcginley.com/sorting-visualizer/",
+                    gh_link:
+                        "https://github.com/cameronmcginley/sorting-visualizer/",
                     description:
                         "React JS application that visualizes various sorting algorithms, such as bubble sort, merge sort, and radix sort. Provides a set of configurations, including delay and array length, and even sound that matches the frequency of sorts!",
                     image: "project-sortingvisualizer-gif.gif",
@@ -61,7 +66,9 @@ const Projects = (props) => {
             <ProjectCard
                 cardData={{
                     title: "Movie Recommender",
-                    link: "https://github.com/cameronmcginley/MovieRecommender",
+                    project_link: "",
+                    gh_link:
+                        "https://github.com/cameronmcginley/MovieRecommender/",
                     description:
                         "Data science course project to build a recommender system without AI. Cleans a 70k+ IMDb movie dataset and standardizes all fields. Recommender collects a user's n favorite movies, then uses a custom algorithm to find their n best movie recommendations. Allows a user to modify weights, e.g. a user can place more or less emphasis on genre or several other fields as they wish. A random class survey showed successful recommendations at x% using default weights, where a successful recommendation is rated as good by the user, or if the user has seen it already (must be a good recommendation if they were willing to watch it!). Pandas allows for vectorization and quick runtimes.",
                     image: "project-movierecommender-image2.png",
@@ -85,7 +92,10 @@ const Projects = (props) => {
             <ProjectCard
                 cardData={{
                     title: "Roblox Object Placement System",
-                    link: "https://github.com/cameronmcginley/RBLX-PlacementSystem",
+                    project_link:
+                        "https://web.roblox.com/games/7619626087/Placement-System-Demo",
+                    gh_link:
+                        "https://github.com/cameronmcginley/RBLX-PlacementSystem/",
                     description:
                         "Tried my hand at Roblox development and built an object placement system. This makes use of raycasting and efficient client-server communications for choosing object and location via client, and building via server. Uses optimal object design to allow for easy additions of new objects and how they interact with the world (e.g. custom hitboxes). Additionally, it saves placed objects per account. Project built in a Rojo enviroment to synchrozine external codebase into Roblox Studio to allow for Git version control.",
                     image: "project-rblxplacement-image.png",
@@ -109,7 +119,9 @@ const Projects = (props) => {
             <ProjectCard
                 cardData={{
                     title: "This Website!",
-                    link: "https://github.com/cameronmcginley/cameronmcginley.github.io",
+                    project_link: "https://cameronmcginley.com/",
+                    gh_link:
+                        "https://github.com/cameronmcginley/cameronmcginley.github.io/",
                     description:
                         "A personal portfolio website, mainly designed to showcase a quick 'about me' section along with various projects. Built using React and Javascript.",
                     image: "project-personalwebsite-image.png",
